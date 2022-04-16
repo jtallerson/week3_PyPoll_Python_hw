090b18a50e28cb7bf9ff7abc1ce446a6d732707c
Jared Allerson <jtallerson@gmail.com>
2022-04-16 10:56:01 -0500

    3.6.4

--- PyPoll Analysis/PyPoll.py
+++ PyPoll Analysis/PyPoll.py
@@ -53,60 +53,79 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    
-    # To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-
-    # Determine winning vote count and candidate
-    # 1. Determine if the votes are greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-            # 2. If true then set winning_count = votes and winning_percent =
-            # vote_percentage.
-            winning_count = votes
-            winning_percentage = vote_percentage
-            # 3. Set the winning_candidate equal to the candidate's name.
-            winning_candidate = candidate_name
-    
-    
-    
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
-
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
-
-
-# Print the candidate vote dictionary.
-print(candidate_votes)
-
-# Print the candidate list.
-#print(candidate_options)
-
-# 3. Print the total votes.
-print(total_votes)
-    # Print the file object.
-    #print(election_data)
-
-
-# Using the with statement open the file as a text file.
+        # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        
+            #print(winning_candidate_summary)
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
 
-    # Write three counties to the file.
-    txt_file.write("Counties in the election\n-------------------------\nArapahoe\nDenver\nJefferson")
+        # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+
+        # Determine winning vote count and candidate
+        # 1. Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+                # 2. If true then set winning_count = votes and winning_percent =
+                # vote_percentage.
+                winning_count = votes
+                winning_percentage = vote_percentage
+                # 3. Set the winning_candidate equal to the candidate's name.
+                winning_candidate = candidate_name
+        
+        
+        
+        # 4. Print the candidate name and percentage of votes.
+        #print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+     # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
+
+    # Print the candidate vote dictionary.
+    #print(candidate_votes)
+
+    # Print the candidate list.
+    #print(candidate_options)
+
+    # 3. Print the total votes.
+    #print(total_votes)
+        # Print the file object.
+        #print(election_data)
+
+
+    # Using the with statement open the file as a text file.
+    with open(file_to_save, "w") as txt_file:
+
+        # Write three counties to the file.
+        txt_file.write("Counties in the election\n-------------------------\nArapahoe\nDenver\nJefferson")
 
 
 
@@ -114,11 +133,11 @@ with open(file_to_save, "w") as txt_file:
 
 
 
-#1. The total number of votes cast.
-#print(len(election_data.readlines()) - 1)
-#2. A complete list of candidates who received votes.
-#3. The percentage of votes each candidate won.
-#4. The total number of votes each candidate won.
-#5. The winner of the election based on popular vote.
+    #1. The total number of votes cast.
+    #print(len(election_data.readlines()) - 1)
+    #2. A complete list of candidates who received votes.
+    #3. The percentage of votes each candidate won.
+    #4. The total number of votes each candidate won.
+    #5. The winner of the election based on popular vote.
 
-# Assign a variable for the file to load and the path.
+    # Assign a variable for the file to load and the path.
